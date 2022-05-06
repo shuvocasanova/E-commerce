36f7ce191553b0adb7052ce130b26707e7092744
shuvocasanova <shuvocasanova@gmail.com>
2022-05-06 16:33:27 +0600

    ..

--- ECOMMERCEAPI/index.js
+++ ECOMMERCEAPI/index.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 
 
-
+mongoose.connect(
+    "mongodb+srv://shuvo140:BzVl9i9aMCfozpjI@cluster0.bgfun.mongodb.net/ecommerce?retryWrites=true&w=majority"
+).then(() => console.log("DB connection succesful")).catch((err)=>console.log(err));
 
 app.listen(5000, () => {
     console.log("backend is running");
