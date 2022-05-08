bff1181f54b8f56f7a886246e41a2e6cac31e5b6
shuvocasanova <shuvocasanova@gmail.com>
2022-05-09 01:04:58 +0600

    Product Page

--- ecommerce-front_end/src/App.js
+++ ecommerce-front_end/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ProductList from "./pages/ProductList";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 const App = () => {
   return (
     <div>
-      <ProductList />
+      {/* <Home/> */}
+      {/* <ProductList /> */}
+      <Product />
     </div>
   );
 };
